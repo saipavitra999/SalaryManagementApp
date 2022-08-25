4eef920577e820637193391d2b6736c5d5796d48
Sai Pavitra <saipavitra@Sais-MacBook-Pro.local>
2022-08-25 22:10:09 +0800

    added sorting for columns in employee table

--- src/components/employeeDashboard.js
+++ src/components/employeeDashboard.js
@@ -32,11 +32,88 @@ let dummyEmployees = [
     fullname: "Ross Geller",
     salary: 5000.0,
   },
+  {
+    id: "id1",
+    username: "user1",
+    fullname: "Monica Geller",
+    salary: 1000.0,
+  },
+  {
+    id: "id2",
+    username: "user2",
+    fullname: "Chandler Bing",
+    salary: 1000.5,
+  },
+  {
+    id: "id3",
+    username: "user3",
+    fullname: "Rachel Green",
+    salary: 0.0,
+  },
+  {
+    id: "id4",
+    username: "user4",
+    fullname: "Ross Geller",
+    salary: 5000.0,
+  },
+  {
+    id: "id1",
+    username: "user1",
+    fullname: "Monica Geller",
+    salary: 1000.0,
+  },
+  {
+    id: "id2",
+    username: "user2",
+    fullname: "Chandler Bing",
+    salary: 1000.5,
+  },
+  {
+    id: "id3",
+    username: "user3",
+    fullname: "Rachel Green",
+    salary: 0.0,
+  },
+  {
+    id: "id4",
+    username: "user4",
+    fullname: "Ross Geller",
+    salary: 5000.0,
+  },
+  {
+    id: "id1",
+    username: "user1",
+    fullname: "Monica Geller",
+    salary: 1000.0,
+  },
+  {
+    id: "id2",
+    username: "user2",
+    fullname: "Chandler Bing",
+    salary: 1000.5,
+  },
+  {
+    id: "id3",
+    username: "user3",
+    fullname: "Rachel Green",
+    salary: 0.0,
+  },
+  {
+    id: "id4",
+    username: "user4",
+    fullname: "Ross Geller",
+    salary: 5000.0,
+  },
 ];
 
 function EmployeeDashboard({ ...props }) {
   const [employeeList, setEmployeeList] = useState(dummyEmployees);
   const [showModal, setShowModal] = useState(false);
+  const [sortColumn, setSortColumn] = React.useState();
+  const [sortType, setSortType] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [limit, setLimit] = React.useState(10);
+  const [page, setPage] = React.useState(1);
   const [values, setValues] = useState({
     employeeName: null,
     employeeId: null,
@@ -44,6 +121,36 @@ function EmployeeDashboard({ ...props }) {
     employeeSalary: null,
     modalType: null,
   });
+
+  const getData = () => {
+    if (sortColumn && sortType) {
+      return employeeList.sort((a, b) => {
+        let x = a[sortColumn];
+        let y = b[sortColumn];
+        if (typeof x === "string") {
+          x = x.charCodeAt();
+        }
+        if (typeof y === "string") {
+          y = y.charCodeAt();
+        }
+        if (sortType === "asc") {
+          return x - y;
+        } else {
+          return y - x;
+        }
+      });
+    }
+    return employeeList;
+  };
+
+  const handleSortColumn = (sortColumn, sortType) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSortColumn(sortColumn);
+      setSortType(sortType);
+    }, 500);
+  };
 
   function modalpopup(data, modalType) {
     setShowModal(true);
@@ -67,7 +174,6 @@ function EmployeeDashboard({ ...props }) {
   }
 
   const onChangeInput = (e) => {
-    console.log("test 123");
     setValues({
       ...values,
       [e.target.name]: e.target.value,
@@ -86,20 +192,28 @@ function EmployeeDashboard({ ...props }) {
     <div>
       <h3>Employee Salary Management Dashboard</h3>
       <h5>Employee List</h5>
-      <Table autoHeight headerHeight={50} data={employeeList}>
-        <Column width={200}>
+      <Table
+        autoHeight
+        headerHeight={50}
+        data={getData()}
+        loading={loading}
+        sortColumn={sortColumn}
+        sortType={sortType}
+        onSortColumn={handleSortColumn}
+      >
+        <Column width={200} sortable>
           <HeaderCell>Id</HeaderCell>
           <Cell dataKey="id" />
         </Column>
-        <Column width={300}>
+        <Column width={300} sortable>
           <HeaderCell>Username</HeaderCell>
           <Cell dataKey="username" />
         </Column>
-        <Column width={300}>
+        <Column width={300} sortable>
           <HeaderCell>Full Name</HeaderCell>
           <Cell dataKey="fullname" />
         </Column>
-        <Column width={300}>
+        <Column width={300} sortable>
           <HeaderCell>Salary</HeaderCell>
           <Cell dataKey="salary" />
         </Column>
